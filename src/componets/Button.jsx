77d47970302ffe693c1fd4264e72719139ac8f21
hagleyson <hagleyson@gmail.com>
2021-01-29 23:30:00 -0300

    primeira versao

---
+++ src/componets/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import "./button.css"
+
+export default props =>{
+    return(
+    <button 
+    onClick={_=> props.click && props.click(props.label)}
+    className={`
+    button
+    ${props.operation ? 'operation':''}
+    ${props.double ? 'double':''}
+    ${props.triple ? 'triple':''}
+    `}>{props.label}</button>
+    )
+}
